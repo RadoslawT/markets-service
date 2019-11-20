aff2aed80f81448d2f1f09a7bbc666c78503dbad
RadoslawT <radoslawtomczynski@gmail.com>
2019-11-20 19:35:24 +0100

    Markets-services: schedule Sidekiq job for market prices update

---
+++ app/application/services/update_bittrex_markets_prices.rb
@@ -0,0 +1,8 @@
+module Services
+  class UpdateBittrexMarketsPrices
+    include Sidekiq::Worker
+
+    def perform
+    end
+  end
+end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,9 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://markets-service-redis:6379/0' }
 end
+
+schedule_file = 'config/schedule.yml'
+
+Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) && Sidekiq.server?
+
+Sidekiq.options[:poll_interval] = 5
