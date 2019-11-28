492a76f5e002148d94124f6841d4be54cd53b12c
RadoslawT <radoslawtomczynski@gmail.com>
2019-11-28 21:05:44 +0100

    Markets-service: add Task entity

---
+++ app/domain/entities/task.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Entities
+  # :nodoc:
+  class Task < Entity
+    params :id, :uuid, :market_uuid, :type, :completion_price, :created_at, :updated_at
+
+    attr_writer :price
+
+    def self.create(params)
+      task_type = ValueObject::TaskType.new(params[:type])
+
+      new(
+        uuid: SecureRandom.uuid,
+        market_uuid: params[:market_uuid],
+        type: task_type.value,
+        completion_price: params[:completion_price]
+      )
+    end
+  end
+end
