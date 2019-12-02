e6087e0282e69404a0933c08ae34989e17b6e89c
RadoslawT <radoslawtomczynski@gmail.com>
2019-12-02 18:43:48 +0100

    Markets-service: add tasks completion to Market aggregate

--- app/domain/aggregates/market.rb
+++ app/domain/aggregates/market.rb
@@ -11,6 +11,16 @@ module Aggregates
     end
 
     def update_price(price)
+      return if @root.price == price
+
+      tasks_to_complete = if price < @root.price
+                            drop_tasks(price)
+                          else
+                            hit_tasks(price)
+                          end
+
+      tasks_to_complete.each(&:complete)
+
       @root.price = price
       nil
     end
@@ -26,10 +36,34 @@ module Aggregates
       nil
     end
 
+    def emit_task_completed_events
+      completed_tasks.each { |t| emit_task_completed_event(t) }
+    end
+
     private
 
     def task_exists?(type, completion_price)
       @tasks.select { |t| t.type == type && t.completion_price == completion_price }.any?
+    end
+
+    def drop_tasks(price)
+      @tasks.select { |t| t.type_drop? && t.completion_price >= price }
+    end
+
+    def hit_tasks(price)
+      @tasks.select { |t| t.type_hit? && t.completion_price <= price }
+    end
+
+    def completed_tasks
+      @tasks.select(&:completed?)
+    end
+
+    def emit_task_completed_event(task)
+      Events::MarketTaskCompleted.call(
+        market_uuid: task.market_uuid,
+        completion_price: task.completion_price,
+        type: task.type.to_s
+      )
     end
   end
 end
