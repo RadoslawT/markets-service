7eaae2abd4b357461cc248f3f5b6e097f913ede1
RadoslawT <radoslawtomczynski@gmail.com>
2019-12-02 21:29:33 +0100

    Markets-service: updatee Task entity spec

--- spec/domain/entities/task_spec.rb
+++ spec/domain/entities/task_spec.rb
@@ -27,4 +27,67 @@ describe Entities::Task do
       end
     end
   end
+
+  describe '#type_drop?' do
+    subject { task.type_drop? }
+
+    let(:task) { described_class.create(params) }
+
+    context 'when type drop' do
+      let(:type) { ValueObjects::TaskType::DROP }
+
+      it { is_expected.to be true }
+    end
+
+    context 'when type hit' do
+      let(:type) { ValueObjects::TaskType::HIT }
+
+      it { is_expected.to be false }
+    end
+  end
+
+  describe '#type_hit?' do
+    subject { task.type_hit? }
+
+    let(:task) { described_class.create(params) }
+
+    context 'when type hit' do
+      let(:type) { ValueObjects::TaskType::HIT }
+
+      it { is_expected.to be true }
+    end
+
+    context 'when type drop' do
+      let(:type) { ValueObjects::TaskType::DROP }
+
+      it { is_expected.to be false }
+    end
+  end
+
+  describe '#complete' do
+    subject(:complete) { task.complete }
+
+    let(:task) { described_class.from_repository(params) }
+
+    it 'delets the task' do
+      complete
+      expect(task.deleted?).to be true
+    end
+  end
+
+  describe '#completed?' do
+    subject(:complete) { task.completed? }
+
+    let(:task) { described_class.from_repository(params)  }
+
+    context 'when task is completed' do
+      before { task.complete }
+
+      it { is_expected.to be true }
+    end
+
+    context 'when task is not completed' do
+      it { is_expected.to be false }
+    end
+  end
 end
