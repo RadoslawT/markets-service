5c257de7d01d535828e4fa9deecf1ade93e13015
RadoslawT <radoslawtomczynski@gmail.com>
2019-12-03 14:34:53 +0100

    Markets-service: add GET /v1/markets route

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   mount Sidekiq::Web => '/sidekiq'
+
+  scope('v1', module: 'controllers/v1') do
+    get '/markets' => 'markets#index'
+  end
 end
